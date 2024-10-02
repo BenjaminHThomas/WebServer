c1b69fea8993bfd981b5fd0952b20b7fb25b669c
Okan Koca <oknakoca@gmail.com>
2024-10-02 20:48:56 +0200

    cleanup: removed the .vscode and some comments/unsued includes

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
+/*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:11:44 by bthomas           #+#    #+#             */
-/*   Updated: 2024/10/02 16:05:02 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/10/02 20:47:26 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-#include "EventHandler.hpp"
-#include "Request.hpp"
-
 #include "cluster.hpp"
-#include "config.hpp"
 #include "json.hpp"
 #include "parser.hpp"
 #include <csignal>
@@ -52,9 +46,3 @@ int main(int ac, char **av)
 
 	return 0;
 }
-/* Testing for Reuqest Parsing */
-	// std::string	raw = "GET /login HTTP/1.1\r\nHost: www.example.com\r\nConnection: keep-alive\r\nContent-Length: 29\r\nCache-Control: max-age=0\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\nusername=johndoe&password=12345";
-	// // std::string	raw = "POST /login HTTP/1.1\nHost: www.example.com\nConnection: keep-alive\nContent-Length: 29\nCache-Control: max-age=0\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\nContent-Type: application/x-www-form-urlencoded\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8\nAccept-Encoding: gzip, deflate, br\nAccept-Language: en-US,en;q=0.9\n\nusername=johndoe&password=12345";
-	// Request r(raw);
-	// Request	rrr(r);
-	// rrr.printAll();
