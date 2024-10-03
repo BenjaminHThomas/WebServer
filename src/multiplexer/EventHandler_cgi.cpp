d8df00395351443c0685c97ef8a42bb08c56f306
Okan Koca <oknakoca@gmail.com>
2024-10-03 21:06:39 +0200

    added alarm to cgi exec in case the script runs forever (timeout is 5 secs for now)

--- src/multiplexer/EventHandler_cgi.cpp
+++ src/multiplexer/EventHandler_cgi.cpp
@@ -6,13 +6,15 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 12:35:44 by bthomas           #+#    #+#             */
-/*   Updated: 2024/10/03 17:06:45 by okoca            ###   ########.fr       */
+/*   Updated: 2024/10/03 21:05:52 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "EventHandler.hpp"
 #include "string.h"
+#include <csignal>
 #include <cstdlib>
+#include <unistd.h>
 #include <vector>
 
 static void setPipe(int *fd, int end) {
@@ -21,6 +23,8 @@ static void setPipe(int *fd, int end) {
 		throw (1);
 	}
 }
+
+void	alarm_handler(int _sig) { (void)_sig; throw (1); }
 
 // forks process - child execve's script
 // return true if success -> false if failed
@@ -45,6 +49,11 @@ bool EventHandler::startCGI(int clientFd, std::vector<std::string> arguments) {
 		args[0] = const_cast<char *>(arguments[0].c_str());
 		args[1] = const_cast<char *>(arguments[1].c_str());
 		args[2] = NULL;
+
+		signal(SIGALRM, alarm_handler);
+
+		alarm(5);
+
 		execve(arguments.front().c_str(), args, environ);
 		delete[] args;
 		std::cerr << "Error: could not execute cgi script\n";
